d591f17ce5030972728ea258e7c3472111f5ffde
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-07 02:23:51 +0530

    Update Student.java

--- Day 13/Session 1/Studeent.java
+++ Day 13/Session 1/Studeent.java
@@ -47,3 +47,109 @@ public class Student {
     }
 
 }
+
+2)//repository
+package com.examly.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.model.Student;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student,Integer> {
+
+}
+
+3)//service
+    package com.examly.springapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.Student;
+import com.examly.springapp.repository.StudentRepo;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepo repo;
+
+    public Student addStudent(Student l){
+        return repo.save(l);
+    }
+
+    public List<Student> getAll(){
+        return repo.findAll();
+    }
+
+    public Student getById(int id){
+        if(repo.existsById(id)){
+            return repo.findById(id).get();
+        }else{
+            return null;
+        }
+    }
+
+}
+
+
+
+4)//controller
+    package com.examly.springapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.model.Student;
+import com.examly.springapp.service.StudentService;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+      @PostMapping("/student")
+    public ResponseEntity<Student> add(@RequestBody Student l){
+        Student l2 = service.addStudent(l);
+        if(l2!=null){
+            return new ResponseEntity<>(l2,HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/student")
+    public ResponseEntity<List<Student>> getAll(){
+        List<Student> list = service.getAll();
+        if(!list.isEmpty()){
+           return new ResponseEntity<>(list,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<Student> getId(@PathVariable int studentId){
+        Student l2 = service.getById(studentId);
+        if(l2!=null){
+            return new ResponseEntity<>(l2,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
+
