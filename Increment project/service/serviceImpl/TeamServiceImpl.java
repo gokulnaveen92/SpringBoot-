5cac61ad76beb0c3630921b7b0f4007585198e1f
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-06 23:08:59 +0530

    Create TeamServiceImpl.java

---
+++ Increment project/service/serviceImpl/TeamServiceImpl.java
@@ -0,0 +1,34 @@
+package com.examly.springapp.service.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.entity.Team;
+import com.examly.springapp.repository.TeamRepo;
+import com.examly.springapp.service.TeamService;
+
+@Service
+public class TeamServiceImpl implements TeamService {
+
+    @Autowired
+    private TeamRepo repo;
+
+    @Override
+    public Team addTeam(Team t) {
+        return repo.save(t);
+       
+    }
+
+    @Override
+    public Team updateTeam(Team t, int id) {
+        if(repo.existsById(id)){
+            Team t1 = repo.findById(id).get();
+            t1.setName(t.getName());
+            t1.setMaximumBudget(t.getMaximumBudget());
+            return t1;
+        }else{
+            return null;
+        }
+    }
+
+}
