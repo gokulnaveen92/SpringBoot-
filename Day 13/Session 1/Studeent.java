fb0dc24202c9539ed1de3793fd56eece1d1d16a4
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-07 02:21:08 +0530

    Create Studeent.java

---
+++ Day 13/Session 1/Studeent.java
@@ -0,0 +1,49 @@
+1)//student
+package com.examly.springapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private int studentId;
+    private String studentName;
+    private String email;
+    
+    public Student() {
+    }
+
+    public Student(int studentId, String studentName, String email) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.email = email;
+    }
+    
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
