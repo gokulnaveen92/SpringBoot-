fba0653b2dbc2da63acc8746f292b4b32d02ab2a
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-17 22:04:40 +0530

    Update book.js

--- Day 22/Session 3/book.js
+++ Day 22/Session 3/book.js
@@ -1,23 +1,35 @@
-//write your code here
-function shopping(){
-    this.groceries = [];
-    this.totalPrice=0;
-    
-    this.addItem=function(item,price){
-        let products ={itemname:item,price:price};
-        this.groceries.push(products);
+class Book{
+    constructor(title,author,price,quantity){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+       
+        this.getBookInfo = function(){
+            return "Title: "+this.title+", Author: "+this.author+", Price: $"+this.price+", Quantity: "+this.quantity;
+        }
     }
-
-    this.calculateTotalPrice=function(){
-        this.totalPrice =this.groceries.reduce((sum,product)=>sum+product.price,0)
-        return this.totalPrice;
-    }
-
 }
-
-let shoppingList = new shopping();
-shoppingList.addItem("biscuits",100);
-shoppingList.addItem("softdrinks",100);
-console.log(shoppingList.calculateTotalPrice());
-//console.log(shoppingList.groceries);
-module.exports=shoppingList;
+ 
+inventory = [];
+ 
+function createBook(title,author,price,quantity){
+    return new Book(title,author,price,quantity);
+}
+ 
+function addBookToInventory(title,author,price,quantity){
+    inventory.push(createBook(title,author,price,quantity));
+}
+ 
+function displayInventory(){
+    inventory.forEach(b => console.log(b.getBookInfo()));
+}
+ 
+function calculateTotalValue(){
+    var total = 0;
+    inventory.forEach(b => total += (b.quantity * b.price));
+    return total;
+}
+ 
+module.exports = {Book, addBookToInventory, displayInventory, calculateTotalValue, createBook, inventory}
+has context menu
