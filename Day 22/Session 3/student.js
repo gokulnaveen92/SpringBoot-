030c99ba48dbe3a6730de02901633d133c425ed2
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-17 18:55:23 +0530

    Update student.js

--- Day 22/Session 3/student.js
+++ Day 22/Session 3/student.js
@@ -1,23 +1,33 @@
-//write your code here
-function shopping(){
-    this.groceries = [];
-    this.totalPrice=0;
-    
-    this.addItem=function(item,price){
-        let products ={itemname:item,price:price};
-        this.groceries.push(products);
-    }
+class Student{
+    constructor(name,rollNo,department){
+        this.name = name;
+        this.rollNo = rollNo;
+        this.department=department;
 
-    this.calculateTotalPrice=function(){
-        this.totalPrice =this.groceries.reduce((sum,product)=>sum+product.price,0)
-        return this.totalPrice;
+        this.getStudentInfo=function(){
+            return"Name: "+this.name+", Roll No: "+this.rollNo+", Department: "+this.department;
+        }
     }
-
 }
 
-let shoppingList = new shopping();
-shoppingList.addItem("biscuits",100);
-shoppingList.addItem("softdrinks",100);
-console.log(shoppingList.calculateTotalPrice());
-//console.log(shoppingList.groceries);
-module.exports=shoppingList;
+let studentList=[];
+
+function addStudent(name,rollNo,department){
+    studentList.push(createStudent(name,rollNo,department))
+}
+
+function createStudent(name,rollNo,department){
+    return new Student(name,rollNo,department);
+}
+
+function displayStudentList(){
+    studentList.forEach(student=>
+        console.log(student.getStudentInfo())
+        )
+}
+
+function calculateTotalStudents(){
+    return studentList.length;
+}
+
+module.exports={Student,addStudent,displayStudentList,calculateTotalStudents,createStudent,studentList};
