1e5520ff02a62c8cbc92687d9f17e1f79a670f7d
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-19 00:34:35 +0530

    Create app.js

---
+++ Day 23/Session 3/login/app.js
@@ -0,0 +1,27 @@
+function validateLogin(){
+    let usernmae = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let result = document.getElementById("result");
+
+    let upper = /[A-Z]+/.test(password);
+    let lower = /[a-z]+/.test(password);
+    let dig = /[0-9]+/.test(password);
+    let spl = /[!@#$%&*_]+/.test(password);
+
+    if(usernmae==="" || password===""){
+        result.innerHTML="Please enter both username and password.";
+        result.classList.add("red");
+        return;
+    }else if(usernmae.length<=6){
+        result.innerHTML="Username must be more than 6 characters.";
+        result.classList.add("red");
+        return;
+    }else if(upper && lower && dig && spl){
+        result.innerHTML="Login successful.";
+        result.classList.add("green");
+    }else{
+        result.innerHTML="Invalid password.";
+        result.classList.add("red");
+    }
+
+}
