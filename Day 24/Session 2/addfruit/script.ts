7e997515f1008f312bb8901f483c2d0a33e526fc
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-20 21:40:42 +0530

    Update script.ts

--- Day 24/Session 2/addfruit/script.ts
+++ Day 24/Session 2/addfruit/script.ts
@@ -1,16 +1,15 @@
+let fruits:string[]=[];
+ 
 function addFruit(){
     let fruit:string = (<HTMLInputElement>document.getElementById("fruitInput")).value;
-    let arr1 : string[]=[];
-
+   
     if(!fruit){
         document.getElementById("errorMessage").innerHTML="Fruit name is required.";
         return;
     }
-    
-    arr1 = fruit.split(",");
-    document.getElementById("errorMessage").innerHTML="";
-
-    document.getElementById("fruitList").innerHTML="Fruits: "+arr1;
-
-
+ 
+    fruits.push(fruit);
+    let strfruit:string = fruits.join(",");
+ 
+    document.getElementById("fruitList").innerHTML="Fruits: "+strfruit;
 }
