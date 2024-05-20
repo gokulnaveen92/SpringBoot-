f9bea8ce799197f24fe0866c56072731c8e64625
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-20 16:29:54 +0530

    Create index.java

---
+++ Day 24/Session 2/addfruit/index.java
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+</head>
+<body>
+    <h1>Fruit Basket</h1><br>
+    <div>
+        <input type="text" id="fruitInput" placeholder="Enter a fruit">
+        <button onclick="addFruit()" id="addFruitButton">Add Fruit</button><br>
+        <div>
+            <span id="errorMessage"></span><br>
+            <span id="fruitList"></span>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
+
+
