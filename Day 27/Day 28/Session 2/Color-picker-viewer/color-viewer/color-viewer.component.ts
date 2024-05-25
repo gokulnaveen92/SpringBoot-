ff4ff03544d5c8f64699176404d4d7b1aa84bd65
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-26 02:32:44 +0530

    Create color-viewer.component.ts

---
+++ Day 27/Day 28/Session 2/Color-picker-viewer/color-viewer/color-viewer.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-viewer',
+  templateUrl: './color-viewer.component.html',
+  styleUrls: ['./color-viewer.component.css']
+})
+export class ColorViewerComponent implements OnInit {
+
+  @Input() receivedColor:string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
