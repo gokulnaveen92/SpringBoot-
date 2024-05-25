9ea369a2ea94de89f8c013f04341217eab798b46
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-25 10:47:40 +0530

    Create searchfilter.pipe.ts

---
+++ Day 27/Session 3/searchTerm/searchfilter.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchFilter'
+})
+export class SearchFilterPipe implements PipeTransform {
+
+  transform(items:any[],searchTerm:string): any[] {
+    if(items.length>0 && searchTerm){
+      var newARR:any[] = items.filter(item=>JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
+      return newARR;
+    }else{
+      return items;
+    }
+  }
+
+}
