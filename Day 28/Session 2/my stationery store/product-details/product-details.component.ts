f9359d75b967c61a78a46058c7d0bb900019836a
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-05-26 23:21:28 +0530

    Create product-details.component.ts

---
+++ Day 28/Session 2/my stationery store/product-details/product-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  @Input() selectedProduct:any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
