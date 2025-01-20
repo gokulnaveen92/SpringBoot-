192ef2ef0c58131abe46e6f2457ac66b46fb33da
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2025-01-20 11:07:59 +0530

    Delete Job Application Management directory

--- Job Application Management/Job-Application-Management/src/main/java/com/example/Job/Application/Management/Repository/InterviewRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Job.Application.Management.Repository;
-
-import com.example.Job.Application.Management.Entity.Interview;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InterviewRepo extends JpaRepository<Interview , Long> {
-}
